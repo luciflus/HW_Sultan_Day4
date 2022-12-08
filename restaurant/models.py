9481327691cc560164944b2d6ce75503baf48bb9
Sultan Usmanov <susmanov@hostkey.com>
2022-12-09 01:59:31 +0600

    additional files was added

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,50 +1,39 @@
 from django.db import models
 
 class User(models.Model):
-    email = models.EmailField(default=0)
-    password = models.CharField(max_length=128, default=0)
+    email = models.EmailField()
+    password = models.CharField(max_length=30)
 
-
-    def __str__(self):
-        return self.email
-
-    class Meta:
-        abstract = True
-        ordering = ['name', ]
-
-class Client(User):
+class Client(models.Model):
     name = models.CharField(max_length=20)
-    card_number = models.CharField(max_length=128)
-
-    class Meta(User.Meta):
-        pass
+    card_number = models.CharField(max_length=20)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
-
-class Worker(User):
+class Worker(models.Model):
     name = models.CharField(max_length=20)
     position = models.CharField(max_length=128)
-
-    class Meta:
-        ordering = ['name', ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.name} в должности {self.position}'
-
+        return self.name
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=128)
-    extra_price = models.FloatField()
+    name = models.CharField(max_length=20)
+    extra_price = models.IntegerField(verbose_name='Надбавка стоимости')
+    calories = models.IntegerField(verbose_name='Колл-во каллорий')
 
     def __str__(self):
         return self.name
 
 class Food(models.Model):
-    name = models.CharField(max_length=128)
-    start_price = models.FloatField()
-    dishes = models.ManyToManyField(Ingredient, related_name='foods', through='Order')
+    name = models.CharField(max_length=128, verbose_name='Наименование блюдо')
+    start_price = models.IntegerField(verbose_name='Изначальная стоимость')
+    type_of_cuisine = models.CharField(max_length=20, verbose_name='Тип кухни')
+    calories = models.IntegerField(verbose_name='Колл-во каллорий')
+    dishes = models.ManyToManyField(Ingredient, through='Order')
 
     def __str__(self):
         return self.name
@@ -54,9 +43,19 @@ class Order(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     worker = models.ForeignKey(Worker, on_delete=models.CASCADE)
+    vegetarian = models.BooleanField(default=False, verbose_name='Вегетарианская')
+    food_status = models.CharField(max_length=20)
+    final_price = models.IntegerField(default=0)
     order_date_time = models.DateField(auto_now=True)
 
     def __str__(self):
-        return f'{self.food.name} - {self.ingredient.name}'
+        return f'{self.food} - {self.ingredient} - {self.client} - {self.worker}'
 
+class ProxyOrder(Order):
+    class Meta:
+        proxy = True
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
+    def vegetarian(self):
+        return self.vegetarian
--- shell.py
+++ shell.py
@@ -1,28 +1,44 @@
-from restaurant.models import User, Client, Ingredient, Food, Order, Worker
+from restaurant.models import User, Client, Ingredient, Food, Order, Worker, ProxyOrder
 
-#user_1 = Client.objects.create(email='nikname21@gmail.com', password='defender42')
-# user_1 = Client.objects.filter(email='nikname21@gmail.com', password='defender42')
-# user_1 = user_1[0]
-# user_1.name='Азат Соколов'
-# user_1.card_number='4147 5657 9878 9009'
-# user_1.save()
+user_1 = User.objects.create(email='nikname21@gmail.com', password='defender42')
+client = Client.objects.create(name='Нурсултан Бердиев', card_number='4147 5657 9878 9009', user=user_1)
+#
+user_2 = User.objects.create(email='altywa1998 @ gmail.com', password='nono34')
+worker = Worker.objects.create(name='Алтынай Алиева', position='Оператор кассы', user=user_2)
+# #
+f1 = Food.objects.create(name='Шаурма', start_price=200, type_of_cuisine='фастфуд', calories=500)
+f2 = Food.objects.create(name='Гамбургер', start_price=180, type_of_cuisine='фастфуд', calories=350)
+f3 = Food.objects.create(name='Паста', start_price=450, type_of_cuisine='Итальянская', calories=400)
+f4 = Food.objects.create(name='Суши', start_price=400, type_of_cuisine='Японская', calories=450)
+f5 = Food.objects.create(name='Боул', start_price=600, type_of_cuisine='Европейское', calories=500)
+# #
+i1 = Ingredient.objects.create(name='сыр', extra_price=80, calories=150)
+i2 = Ingredient.objects.create(name='курица', extra_price=100, calories=250)
+i3 = Ingredient.objects.create(name='говядина', extra_price=120, calories=300)
+i4 = Ingredient.objects.create(name='рыба', extra_price=120, calories=270)
+i5 = Ingredient.objects.create(name='рис', extra_price=70, calories=100)
+i6 = Ingredient.objects.create(name='творог', extra_price=100, calories=170)
+i7 = Ingredient.objects.create(name='куриные яйца', extra_price=50, calories=120)
+i8 = Ingredient.objects.create(name='салат', extra_price=50, calories=50)
+i9 = Ingredient.objects.create(name='фри', extra_price=50, calories=70)
 
-# user_2 = Worker.objects.create(email='altywa1998 @ gmail.com', password='nono34')
-# user_2 = Worker.objects.filter(email='altywa1998 @ gmail.com', password='nono34')
-# user_2 = user_2[0]
-# user_2.name = 'Алтынай Алиева'
-# user_2.position = 'Оператор кассы'
-# user_2.save()
+f1.dishes.set([i3, i1, i8, i9], through_defaults={'client': client, 'worker': worker})
+f2.dishes.set([i2, i8], through_defaults={'client': client, 'worker': worker})
+f3.dishes.set([i8, i7, i1, i6], through_defaults={'client': client, 'worker': worker})
+f4.dishes.set([i5, i1, i9, i8, i6], through_defaults={'client': client, 'worker': worker})
+f5.dishes.set([i5, i4, i7], through_defaults={'client': client, 'worker': worker})
+#
+f3.dishes.set([i1, i9], through_defaults={'client': client, 'worker': worker})
+f4.dishes.set([i1, i4, i5], through_defaults={'client': client, 'worker': worker})
+f1.dishes.set([i1, i3], through_defaults={'client': client, 'worker': worker})
+f2.dishes.set([i1, i2], through_defaults={'client': client, 'worker': worker})
+f5.dishes.set([i1, i5, i7], through_defaults={'client': client, 'worker': worker})
 
-#f1 = Food.objects.create(name='Шаурма', start_price='50')
-#f2 = Food.objects.create(name='Гамбургер', start_price='25')
+ingridients = Ingredient.objects.all()
+print(ingridients)
+#po = ProxyOrder(vegetarian=1, client=client, worker=worker, food=f1, ingredient=i1)
+for i in ingridients.iterator():
+    if i.name == 'говядина' or i.name == 'курица':
+        po = ProxyOrder(vegetarian=1, client=client, worker=worker)
+        po.save()
 
-# i1 = Ingredient.objects.create(name='сыр', extra_price='10')
-# i2 = Ingredient.objects.create(name='курица', extra_price='70')
-# i3 = Ingredient.objects.create(name='говядина', extra_price='80')
-# i4 = Ingredient.objects.create(name='салат', extra_price='15')
-# i5 = Ingredient.objects.create(name='фри', extra_price='15')
-
-m1 = Order(client=1, worker=1, food='Шаурма', ingredient='говядина')
-
-ValueError: Cannot assign "'Шаурма'": "Order.food" must be a "Food" instance.
